f57e814f4d5da0352a48bb0282ebfdd4775bb8fb
Ayush Shashank <ayush.bibhas@gmail.com>
2023-05-01 08:00:47 +0530

    typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ProductModule,
+    ConfigModule.forRoot({ isGlobal: true, expandVariables: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('DB_HOST', 'localhost'),
+        port: +config.get<number>('DB_PORT', 3306),
+        username: config.get<string>('DB_USERNAME', 'root'),
+        password: config.get<string>('DB_PASSWORD', 'root'),
+        database: config.get<string>('DB_NAME', 'ss_users'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { GlobalExceptionFilter } from './filters/GlobalExceptionFilter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createApplicationContext(AppModule);
+  const config = app.get<ConfigService>(ConfigService);
+  const host = config.get<string>('PRODUCT_HOST', 'localhost');
+  const port = +config.get<number>('PRODUCT_PORT', 3002);
+  const productService = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: host,
+        port: port,
+      },
+    },
+  );
+  productService.useGlobalFilters(new GlobalExceptionFilter());
+  await productService.listen();
 }
 bootstrap();
